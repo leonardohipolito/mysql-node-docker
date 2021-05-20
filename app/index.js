693b267e934c355bad6cd33920b1be3b864df17c
Leonardo Hipolito <leonardohipolito.dev@gmail.com>
2021-05-20 16:09:45 -0300

    wip

---
+++ app/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config)
+    connection.query(`INSERT INTO people(name)
+                      values ('Wesley')`)
+    connection.query(`INSERT INTO people(name)
+                      values ('Leonardo')`)
+    connection.query('SELECT * from people;', function (error, results, fields) {
+        if (error) throw error;
+        res.send('<h1>Full Cycle</h1>' + results.map(result => result.name).join('<br>'))
+    });
+    connection.end()
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
